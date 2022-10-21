c4027f7e3f82033418a14a17df1bd6eca55ac986
Ebi <kpemiogokimiebi@gmail.com>
2022-10-21 20:46:52 +0200

    Make get and post request using fetch API and async & await keywords

--- API-Projects/HTTP&HTTPS/public/main.js
+++ API-Projects/HTTP&HTTPS/public/main.js
@@ -1,5 +1,5 @@
-// Information to reach API
-const apiKey = "<Your API Key>";
+// information to reach API
+const apiKey = "API KEY";
 const url = "https://api.rebrandly.com/v1/links";
 
 // Some page elements
@@ -8,32 +8,25 @@ const shortenButton = document.querySelector("#shorten");
 const responseField = document.querySelector("#responseField");
 
 // Asynchronous functions
-const shortenUrl = () => {
+const shortenUrl = async () => {
   const urlToShorten = inputField.value;
   const data = JSON.stringify({ destination: urlToShorten });
-
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-      apikey: apiKey,
-    },
-    body: data,
-  })
-    .then(
-      (response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Request failed!");
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-type": "application/json",
+        apikey: apiKey,
       },
-      (networkError) => {
-        console.log(networkError.message);
-      }
-    )
-    .then((jsonResponse) => {
-      renderResponse(jsonResponse);
     });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Clear page and call Asynchronous functions
